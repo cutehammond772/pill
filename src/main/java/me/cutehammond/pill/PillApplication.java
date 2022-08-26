f5f1bd21d127fc854e02b4e9d1c98a0991990040
Jungheon Lee <cutehammond772@gmail.com>
2022-08-26 17:23:42 +0900

    - Creating Pill Project

---
+++ src/main/java/me/cutehammond/pill/PillApplication.java
@@ -0,0 +1,13 @@
+package me.cutehammond.pill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/cutehammond/pill/PillApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cutehammond.pill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
